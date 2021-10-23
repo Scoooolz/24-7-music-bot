76d76fc0584eaac8d355f1daf44e49ee0be57929
Scoooolz <84376629+Scoooolz@users.noreply.github.com>
2021-10-23 15:35:43 +0700

    Add multiple channel.

--- index.js
+++ index.js
@@ -6,31 +6,26 @@ const {
     video_urls
 } = require("./config.json");
 const client = new Discord.Client();
-const port = 3000;
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send('Looks your bot is online!');
-});
-
-app.listen(port);
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
-    const voiceChannel = client.channels.cache.get(channel_id);
-    voiceChannel.join().then(connection => {
-        console.log("Joined voice channel");
-        function play(connection) {
-            const stream = ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" });
-            const dispatcher = connection.play(stream);
-            dispatcher.on("finish", () => {
-                play(connection);
-            });
-        };
 
-        play(connection);
-    });
+    channel_id.forEach((more) => {
+        const voiceChannel = client.channels.cache.get(more);
+        voiceChannel.join().then(connection => {
+            console.log("Joined voice channel");
+            function play(connection) {
+                const stream = ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], {    filter: "audioonly" });
+                const dispatcher = connection.play(stream);
+                dispatcher.on("finish", () => {
+                    play(connection);
+               });
+           };
+
+           play(connection);
+        });
+    })
+    
 });
 
 client.login(token);
